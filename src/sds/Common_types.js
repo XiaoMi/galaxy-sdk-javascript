4e8031bd3fea8d30f2b7c77b5815d9638b2c1e54
linshangquan <linshangquan@xiaomi.com>
2015-11-12 09:45:29 +0800

    generate js sdk to support snapshot
    Summary: Ref T4252
    
    Test Plan: N/A
    
    Reviewers: qiankai
    
    Reviewed By: qiankai
    
    Maniphest Tasks: T4252
    
    Differential Revision: https://phabricator.d.xiaomi.net/D24181

--- src/sds/Common_types.js
+++ src/sds/Common_types.js
@@ -13,7 +13,7 @@ ThriftProtocol = {
 Version = function(args) {
   this.major = 1;
   this.minor = 0;
-  this.patch = 'f517e5c6';
+  this.patch = 'd23d26c6';
   this.comments = '';
   if (args) {
     if (args.major !== undefined) {
